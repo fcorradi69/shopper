2404e3a80c794bcc6f2cdcf748be426be5e410c2
fcorradi69 <82023644+fcorradi69@users.noreply.github.com>
2026-01-12 14:02:06 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = 'shopper-pwa-1';
+const ASSETS_TO_CACHE = [
+  'index.html',
+  'style.css',
+  'app.js',
+  'service-worker.js',
+  'manifest.json',
+  'images/background.jpg',
+  'images/icon.png',
+];
+
+// Install: cache dei file base
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// Activate: pulizia cache vecchie (qui semplice)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(
+        keys.map(key => { 
+          if (key !== CACHE_NAME) 
+          { 
+            return caches.delete(key); 
+          } 
+        }) 
+      ) 
+    ) 
+  );
+  //event.waitUntil(self.clients.claim());
+});
+
+// Fetch: strategia cache-first (se manca rete, serve cache) 
+self.addEventListener('fetch', (event) => { 
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Se trovo nella cache, uso quello
+      if (cachedResponse)
+      {
+        return cachedResponse;
+      }
+      
+      // Altrimenti provo rete e (se va) metto in cache
+      return fetch(event.request)
+        .then((networkResponse) => {
+          // Non cachiamo richieste non valide
+          if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic')
+          {
+            return networkResponse;
+          }
+          
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+          
+          return networkResponse;
+        })
+        .catch(() => {
+          // Se anche la rete fallisce e non ho cache, potresti:
+          // - restituire una pagina offline personalizzata
+          // Per ora non restituiamo niente di speciale.
+        });
+    })
+  );
+});
