d8334433dff3726352af3ee2ee901bd0d4a45aca
fcorradi69 <82023644+fcorradi69@users.noreply.github.com>
2026-01-12 15:56:05 +0100

    Update models.js

--- models.js
+++ models.js
@@ -1,1 +1,15 @@
+export class CART {
+  constructor(id, description) {
+    this.CartId = id;
+    this.Cart = description;
+    this.Articles = [];
+  }
+}
 
+export class ARTICLE {
+  ArticleId = 0;
+  Article = "";
+  Prezzo = 0.0;
+  Qta = 1;
+  constructor() { }
+}
